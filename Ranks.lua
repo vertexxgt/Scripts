2358e04aa2ff8e1cf39100b3415541708375c179
famexyzz <140981719+vertexxgt@users.noreply.github.com>
2025-07-24 21:39:17 -0400

    Create Ranks.lua

---
+++ Ranks.lua
@@ -0,0 +1,22 @@
+{
+  "TrustedTraders": {
+    "ghsata": true,
+    "killfaded": true
+  },
+  "Middlemen": {
+    "Neptenie": true,
+    "wthbryce": true,
+    "Pooksterwookster": true,
+    "sadnotaaladin": true,
+    "70kwest": true,
+    "TheRealCris_gamerYT": true,
+    "etanalop": true,
+    "pogls800": true,
+    "4spy0": true,
+    "ninjafantom_y": true,
+    "shrekiscoolbtw": true,
+    "MarGamesALot": true,
+    "63_ycey1": true,
+    "Engizeer": true
+  }
+}
