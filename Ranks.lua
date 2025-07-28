ea985d54dc7a8b5ae37c618aea49b51b08a83aca
famexyzz <140981719+vertexxgt@users.noreply.github.com>
2025-07-27 21:24:11 -0400

    Update Ranks.lua

--- Ranks.lua
+++ Ranks.lua
@@ -2,20 +2,5 @@
   "TrustedTraders": {
     "ghsata": true,
     "killfaded": true
-  },
-  "Middleman": {
-    "Neptenie": true,
-    "Pooksterwookster": true,
-    "sadnotaaladin": true,
-    "70kwest": true,
-    "TheRealCris_gamerYT": true,
-    "etanalop": true,
-    "pogls800": true,
-    "4spy0": true,
-    "ninjafantom_y": true,
-    "shrekiscoolbtw": true,
-    "MarGamesALot": true,
-    "63_ycey1": true,
-    "Engizeer": true
   }
 }
